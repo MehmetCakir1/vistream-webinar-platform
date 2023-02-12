e2788060e11ede05402ffcacbf031d5a1732f769
MehmetCakir1 <mehmetcakir4227@gmail.com>
2023-02-12 21:13:42 +0300

    countdown

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -3,7 +3,7 @@ import { speakersData } from '../utils/speakerData'
 import SingleSpeaker from './SingleSpeaker'
 
 const Speakers = () => {
-  const [date,setDate]=useState("Wednesday, 25 February 2021")
+  const [date,setDate]=useState("Wednesday, 25 February 2023")
   const currentSpeakers=speakersData.filter(item=>item.date==date)
 
   const changeTheDate=(e)=>{
@@ -14,12 +14,12 @@ const Speakers = () => {
     <main>
       <h1 className='text-center text-3xl md:text-[40px] pt-7 pb-2 text-[#232B76] font-semibold'>SPEAKERS</h1>
       <div className='flex justify-center items-center gap-5 py-2'>
-        <button className={`${date=="Wednesday, 25 February 2021" ? "bg-[#232B76] text-white " : "bg-white text-[#232B76] border border-[#232B76]"} rounded-3xl md:px-5 py-2 transition-all 800ms ease-linear text-[12px] md:text-[16px] px-2`}
+        <button className={`${date=="Wednesday, 25 February 2023" ? "bg-[#232B76] text-white " : "bg-white text-[#232B76] border border-[#232B76]"} rounded-3xl md:px-5 py-2 transition-all 800ms ease-linear text-[12px] md:text-[16px] px-2`}
         onClick={(e)=>changeTheDate(e)}
-        >Wednesday, 25 February 2021</button>
-        <button className={`${date=="Wednesday, 26 February 2021" ? "bg-[#232B76] text-white " : "bg-white text-[#232B76] border border-[#232B76]"} rounded-3xl md:px-5 py-2 transition-all 800ms ease-linear text-[12px] md:text-[16px] px-2`}
+        >Wednesday, 25 February 2023</button>
+        <button className={`${date=="Wednesday, 26 February 2023" ? "bg-[#232B76] text-white " : "bg-white text-[#232B76] border border-[#232B76]"} rounded-3xl md:px-5 py-2 transition-all 800ms ease-linear text-[12px] md:text-[16px] px-2`}
         onClick={(e)=>changeTheDate(e)}
-        >Wednesday, 26 February 2021</button>
+        >Wednesday, 26 February 2023</button>
       </div>
       <section className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center max-w-[1200px] m-auto gap-y-5 gap-x-3 mt-12'>
         {
--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Schedule = () => {
   return (
-    <div className='min-h-[calc(100vh-220px)]'>Schedule</div>
+    <div className='min-h-[calc(100vh-220px)] uppercase font-bold text-3xl flex items-center justify-center'>Schedule</div>
   )
 }
 
