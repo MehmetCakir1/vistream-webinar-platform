fb68e733db668fc5ca3d3456cbc3154ba9f20db6
Mehmet Çakır <101903416+MehmetCakir1@users.noreply.github.com>
2023-02-13 01:45:06 +0300

    Update SingleSpeaker.jsx

--- src/components/SingleSpeaker.jsx
+++ src/components/SingleSpeaker.jsx
@@ -13,7 +13,7 @@ const SingleSpeaker = ({item}) => {
     <div className='flex flex-col justify-center items-center'>
         <img src={img} alt={name} className="object-cover"/>
         <h2 className='py-1 font-bold text-[#232b76]'>{name}</h2>
-        <p className='text-center py-2'>{desc}</p>
+        <p className='text-center py-2 text-[#333333]'>{desc}</p>
         <div className='h-[3rem] flex items-center justify-center'>
           <button className='bg-[#232b76] text-white py-1 px-3 rounded-2xl hover:bg-white hover:text-[#232b76] hover:border border-[#232b76]'
           onClick={goToBioPage}
